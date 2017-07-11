d7e723560edd20a51776c6775bcb712d491555ce
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-07-11 18:19:04 +0700

    working

--- release.sh
+++ release.sh
@@ -1,7 +1,12 @@
+# Ensure git flow exist
+command -v git-flow >/dev/null 2>&1 || brew install git-flow-avh
+
+# Tag me
 tag=$1
 tag=${tag:-$(date +%s)}
 echo $tag > ./msg.tmp
 
+# Start release
 git checkout develop
 git push
 git flow release start $tag
