b0af11ab43608982aacc72300975102d64c5ddf3
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-07-11 17:19:44 +0700

    booya

--- release.sh
+++ release.sh
@@ -3,8 +3,10 @@ tag=${tag:-$(date +%s)}
 echo $tag > ./msg.tmp
 
 git checkout develop
+git push
 git-flow release start $tag
 git-flow release finish -f ./msg.tmp $tag
+rm -f ./msg.tmp
 git checkout master
 git push
 git checkout develop
