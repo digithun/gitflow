966525b2ac9c55c920e6940eb35b6613ffcf6ca3
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-07-11 18:20:32 +0700

    try -m

--- release.sh
+++ release.sh
@@ -10,8 +10,7 @@ echo $tag > ./msg.tmp
 git checkout develop
 git push
 git flow release start $tag
-git flow release finish $tag -f ./msg.tmp
-rm -f ./msg.tmp
+git flow release finish -m $tag $tag
 git checkout master
 git push
 git checkout develop
