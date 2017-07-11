e3ce193c408fc7bf9c84b6829c9c67036e42d98a
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2017-07-11 17:15:54 +0700

    should work

--- release.sh
+++ release.sh
@@ -1,11 +1,8 @@
-tag=$1
-msg=$2
-
-tag=${tag:-$(date +%s)}
-msg=${msg:-"Release"}
+tag=${$1:-$(date +%s)}
+echo $tag > ./msg.tmp
 
 git-flow release start $tag
-git-flow release finish -m $msg $tag
+git-flow release finish -f ./msg.tmp $tag
 git checkout master
 git push
 git checkout develop
